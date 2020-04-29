4395908e97fe0dc0f6e08497ad21a8d5dc5d95cc
aengussong <oleg.durasov.2016@gmail.com>
2020-04-29 18:13:07 +0300

    add minor adjustments

--- app/src/androidTest/java/com/aengussong/leisuretime/util/MockKoinModules.kt
+++ app/src/androidTest/java/com/aengussong/leisuretime/util/MockKoinModules.kt
@@ -3,10 +3,11 @@ package com.aengussong.leisuretime.util
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import com.aengussong.leisuretime.data.local.LeisureDb
+import com.aengussong.leisuretime.di.dataModule
 import org.koin.dsl.module
 
 val mockDbModule = module {
-    single {
+    single(override = true) {
         Room.inMemoryDatabaseBuilder(
             ApplicationProvider.getApplicationContext(),
             LeisureDb::class.java
