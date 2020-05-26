a1112b243837543bb1e1b1a72b75c64de9dc0665
Oleg Durasov <oleg.durasov.2016@gmail.com>
2020-05-26 12:04:35 +0300

    add test to check data load on start (#7)

--- app/src/androidTest/java/com/aengussong/leisuretime/viewModel/LeisureDataViewModelTest.kt
+++ app/src/androidTest/java/com/aengussong/leisuretime/viewModel/LeisureDataViewModelTest.kt
@@ -1,22 +1,27 @@
 package com.aengussong.leisuretime.viewModel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.aengussong.leisuretime.DbRelatedTest
 import com.aengussong.leisuretime.LeisureDataViewModel
 import com.aengussong.leisuretime.util.getOrAwaitValue
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
-import org.koin.test.KoinTest
 import org.koin.test.inject
 
 
-class LeisureDataViewModelTest : KoinTest {
+class LeisureDataViewModelTest : DbRelatedTest() {
 
     @get:Rule
     val instantTaskRule = InstantTaskExecutorRule()
 
     private val viewModel: LeisureDataViewModel by inject()
+
+    @Test
+    fun startViewModel_dataShouldBeLoaded() {
+        viewModel.leisureLiveData.getOrAwaitValue()
+    }
 
     @Test
     fun addItem_itemShouldBeAdded() = runBlocking {
