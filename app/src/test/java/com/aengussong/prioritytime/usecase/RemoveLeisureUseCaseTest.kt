e8f50bb9bb0944fcf4e567711982825233052934
Oleh Durasov <oleh.durasov@923digital.com>
2021-01-17 21:20:28 +0200

    [Bug] RemoveLeisure: fix task's children not recursively deleted on the task deletion

--- app/src/test/java/com/aengussong/prioritytime/usecase/RemoveLeisureUseCaseTest.kt
+++ app/src/test/java/com/aengussong/prioritytime/usecase/RemoveLeisureUseCaseTest.kt
@@ -27,18 +27,32 @@ class RemoveLeisureUseCaseTest {
     }
 
     @Test
-    fun `remove root leisure - only this root leisure should be deleted`() = runBlocking {
+    fun `remove root leisure - only this root leisure and it's children should be deleted`() = runBlocking {
         val leisure =
             LeisureProvider.getGenericEntity().copy(ancestry = AncestryBuilder().toString())
+        val leisureAncestryForChildren = AncestryBuilder(leisure.ancestry).withChild(leisure.id).toString()
         coEvery { repo.removeLeisure(leisure.id) } just Runs
         coEvery { repo.removeLeisures(any()) } just Runs
         coEvery { repo.getAncestry(leisure.id) } returns leisure.ancestry
 
-
         useCase.execute(leisure.id)
 
         coVerify(exactly = 1) { repo.removeLeisure(leisure.id) }
-        coVerify(exactly = 0) { repo.removeLeisures(any()) }
+        coVerify(exactly = 0) { repo.removeLeisures(neq(leisureAncestryForChildren)) }
+    }
+
+    @Test
+    fun `remove root leisure - root's children also should be removed`() = runBlocking {
+        val parentId = 0L
+        val parentAncestry = AncestryBuilder().withChild(parentId).toString()
+        coEvery { repo.getAncestry(parentId) } returns AncestryBuilder().toString()
+        coEvery { repo.removeLeisure(parentId) } just Runs
+        coEvery { repo.removeLeisures(parentAncestry) } just Runs
+
+        useCase.execute(parentId)
+
+        coVerify { repo.removeLeisure(parentId) }
+        coVerify { repo.removeLeisures(parentAncestry) }
     }
 
 }
